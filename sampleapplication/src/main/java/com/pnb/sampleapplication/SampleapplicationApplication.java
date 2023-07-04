397925684028652e621841f31280f7cedf6ba726
aquinojr <aquinojr@pnb.com.ph>
2023-07-04 13:10:53 +0800

    First Commit

---
+++ sampleapplication/src/main/java/com/pnb/sampleapplication/SampleapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.pnb.sampleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleapplicationApplication.class, args);
+	}
+
+}
